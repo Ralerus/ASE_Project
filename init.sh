32a21ad4ff9f6542d63e8c51b93aa5183088fc18
Philipp <online@philipp-rall.de>
2021-05-28 23:22:06 +0200

    Introduce generic Exceptions for text & player

---
+++ init.sh
@@ -0,0 +1,3 @@
+sudo apt-get install sqlite3
+sudo apt install -y openjdk-11-jre-headless
+sudo apt install -y maven
---
+++ start.sh
@@ -0,0 +1,3 @@
+mvn clean package;
+cd target;
+java -cp Tippduell-1.0 application.Application;
