21b7a3f38b25642691f4783f7eb84bf6cd4c9430
Philipp <online@philipp-rall.de>
2021-05-31 12:46:42 +0200

    Update pom

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 mvn clean package;
 cd target/classes;
 cp ~/.m2/repository/org/xerial/sqlite-jdbc/3.34.0/sqlite-jdbc-3.34.0.jar .
+cp ../../src/main/resources/icon.png layer/presentation
 java -classpath ".:sqlite-jdbc-3.34.0.jar" application.Application
